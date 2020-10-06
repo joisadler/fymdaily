4562bbc572ea8193095c851069d30517b3e262e5
Yossi Adler <joisadler@gmail.com>
2020-10-06 23:28:39 +0300

    remove build directory from gitignore

---
+++ client/build/precache-manifest.016bbe858e521a68f672cc2a6e9cbe66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f9b5b0d45103955925c4ef6235a0365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12acafe776b06f4baafa",
+    "url": "/static/css/2.627926fe.chunk.css"
+  },
+  {
+    "revision": "c75ace6c5483d66ed3d9",
+    "url": "/static/css/main.1d27cc33.chunk.css"
+  },
+  {
+    "revision": "12acafe776b06f4baafa",
+    "url": "/static/js/2.f88d2809.chunk.js"
+  },
+  {
+    "revision": "a79b99580a89d4095facb13fd25b344c",
+    "url": "/static/js/2.f88d2809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75ace6c5483d66ed3d9",
+    "url": "/static/js/main.a010e5a9.chunk.js"
+  },
+  {
+    "revision": "e8f98bcf86eb88b38297",
+    "url": "/static/js/runtime-main.e4beb700.js"
+  }
+]);
